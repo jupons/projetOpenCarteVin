7d6d352df1d50ed9f0485c06a1a945f2308fe345
Valentine-Gripois <124247690+Valentine-Gripois@users.noreply.github.com>
2023-02-09 12:13:34 +0100

    Add files via upload

---
+++ liste_telechargement.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+ui <- fluidPage(
+  
+  titlePanel("Bienvenue sur notre carte des vins !"),
+  textInput("nom", label="Veuillez entrer votre nom", value="Nom"),
+  textOutput(outputId="text"),
+  selectInput(inputId = "idSelect", label = "Veuillez sélectionner l'année du vin souhaitée ", 
+              selected = 10, choices = c("2011"=2011, "2012"=2012, "2013"=2013,
+                                         "2014"=2014, "2015"=2015, "2016"=2016, "2017"=2017,
+                                         "2018"=2018, "2019"=2019, "2020"=2020, "2021"=2021,
+                                         "2022"=2022)),
+  #bouton telechargement
+  hr(),
+  
+  h3("Sauvegarder le graphique"),
+  
+  selectInput(inputId = "ExportGraph",
+              label = "Choisir le format",
+              choices = c("PNG", "JPEG", "PDF"),
+              selected = "PNG"),
+  
+  downloadButton("downloadData2", "Download")
+
+)
+
+server <- function(input, output) {
+  output$text <- renderText({
+    paste("Bienvenue ", input$nom, " !")
+  })
+}
+shinyApp(ui = ui, server = server)
