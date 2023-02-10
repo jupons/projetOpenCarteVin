6c30b80a9d1ce459067b5f4a9b7da97037927d7a
IrisAnif <ianifrani@etu.isara.fr>
2023-02-10 14:12:34 +0100

    Test code.R

--- Test code.R
+++ Test code.R
@@ -3,11 +3,28 @@ library(leaflet)
 library(sf)
 
 ui <- fluidPage(
-  titlePanel("France"),
-  br(),
+  titlePanel("Bienvenue sur notre carte des vins !"),
+  textOutput(outputId="text"),
+  selectInput(inputId = "idSelect", label = "Veuillez sélectionner l'année du vin souhaitée ", 
+              selected = 11, choices = c("2011"=2011, "2012"=2012, "2013"=2013,
+                                         "2014"=2014, "2015"=2015, "2016"=2016, "2017"=2017,
+                                         "2018"=2018, "2019"=2019, "2020"=2020, "2021"=2021,
+                                         "2022"=2022)),
+  #bouton telechargement
+  hr(),
+  
+  h3("Sauvegarder le graphique"),
+  
+  selectInput(inputId = "ExportGraph",
+              label = "Choisir le format",
+              choices = c("PNG", "JPEG", "PDF"),
+              selected = "PNG"),
+  
+  downloadButton("downloadData2", "Download"),
+  
   fluidRow(
-    column(6, leafletOutput("map")),
-    column(6, "")
+    column(8, leafletOutput("map")),
+    column(8, "")
   )
 )
 
@@ -32,7 +49,7 @@ server <- function(input, output) {
                   popup = paste("Département:", departements$nom)) %>%
       addPopups(lng = mean(departements$geometry),
                 lat = mean(departements$geometry),
-                popup = paste("<a href='https://example.com/departements/", departements$nom, "'>", departements$nom, "</a>"))
+                popup = paste(departements$nom, "'>", departements$nom, "</a>"))
     
     })
 }
